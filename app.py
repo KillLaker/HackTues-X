b43e11d997effc5b587725229b592a83cf6bbc00
alt-plus-f4 <valentin.assenov@abv.bg>
2024-03-14 13:16:01 +0200

    Added quiz gen

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
 @app.route("/upload", methods=['POST'])
 def get_uploaded_file():
     uploaded_file = request.files['uploaded-file']
@@ -28,6 +27,43 @@ def get_uploaded_file():
 
     return redirect(url_for('home'))
 
+@app.route("/quiz/<int:quiz>")
+def quiz(quiz):
+
+    # Read the data from the database 
+
+    quiz = {
+        "id": quiz,
+        "title": "Quiz 1",
+        "questions": [
+            {
+                "id": 1,
+                "question": "What is the capital of France?",
+                "options": ["Paris", "London", "Berlin", "Madrid"],
+                "correct": "Paris"
+            },
+            {
+                "id": 2,
+                "question": "What is the capital of Germany?",
+                "options": ["Paris", "London", "Berlin", "Madrid"],
+                "correct": "Berlin"
+            },
+            {
+                "id": 3,
+                "question": "What is the capital of Spain?",
+                "options": ["Paris", "London", "Berlin", "Madrid"],
+                "correct": "Madrid"
+            },
+            {
+                "id": 4,
+                "question": "What is the capital of United Kingdom?",
+                "options": ["Paris", "London", "Berlin", "Madrid"],
+                "correct": "London"
+            }
+        ]
+    }
+
+    return render_template('quiz.html', quiz=quiz)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
