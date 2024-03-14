306b16ec96d1e55499c05162137e4209ecee197b
AvatarZorak <alexhristov2005@gmail.com>
2024-03-14 04:57:20 +0200

    Added uploading page.

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['GET'])
 def home():
     return render_template('index.html')
+
+
+@app.route("/upload", methods=['POST'])
+def get_uploaded_file():
+    uploaded_file = request.files['uploaded-file']
+
+    if 'uploaded-file' not in request.files:
+        return render_template('error_uploading.html')
+
+    print("Filename" + uploaded_file.filename)
+
+    if len(uploaded_file.filename) == 0:
+        return render_template('error_uploading.html')
+
+    app.config['UPLOAD_FOLDER'] = './static/uploads'
+
+    uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'quiz-source.txt'))
+
+    return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
