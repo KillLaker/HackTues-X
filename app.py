e8da485b7144997869e94b5fedf3e02a95689e57
alt-plus-f4 <valentin.assenov@abv.bg>
2024-03-14 23:14:17 +0200

    Reformatted the whole app.py

--- app.py
+++ app.py
@@ -8,14 +8,10 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from jinja2 import Environment, PackageLoader, select_autoescape
 from openaiApi import generate_multiple_choice_questions
 from openai import OpenAI
-# from convert_files_to_txt import convert_to_txt
 from werkzeug.utils import secure_filename
 from convert_files_to_txt import *
 
-import os
-import requests
-
-# TODO fix imports and reorganize code
+# TODO fix imports
 
 # ! move the sensitive data to .env file
 cnx = mysql.connector.connect(
@@ -25,15 +21,64 @@ cnx = mysql.connector.connect(
     database="hacktuesx"
 )
 
-# ! remove this
-cursor = cnx.cursor()
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jagdhsflkuaysdfo718349871' #! os.getenv("SECRET_KEY")
 
 @app.route("/", methods=['GET'])
 def home():
     return render_template('index.html')
+
+# ----------------------------------- #
+#        Login returns jwt token      #
+# ----------------------------------- #
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        try:
+            user = get_user(request.form['username-field'], request.form['password-field'])
+            token = generate_token(user[0], user[3])
+            session['token'] = token
+            return "<h1>Successfully logged in.</h1>"
+
+        except argon2.exceptions.VerifyMismatchError:
+            return jsonify({"message": "Invalid username or password"}), 401
+    else:
+        return render_template("login.html")
+
+# ----------------------------------- #
+#  Returns the user object in the DB  #
+# ----------------------------------- #
+
+def get_user(username, password):
+    cursor = cnx.cursor()
+
+    cursor.execute("select * from Users where user_validator = %s", (username,))
+    user = cursor.fetchone()
+    print(user)
+
+    if argon2.verify_password(bytes(user[2]), password.encode('utf-8')):
+        return user
+
+    return None
+
+# ----------------------------------- #
+#  Returns JWT token from userid      #
+# ----------------------------------- #
+
+def generate_token(id, permission):
+    payload = {
+        "id": id,
+        "permission": permission,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=0.016)
+    }
+
+    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+    return token
+    
+# ----------------------------------- #
+#         UPLOAD FILE FOR QUIZ        #
+# ----------------------------------- #
 
 @app.route("/upload", methods=['POST'])
 def get_uploaded_file():
@@ -43,9 +88,12 @@ def get_uploaded_file():
 
         token = session['token']
         json_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms='HS256')
+        student_id = json_token['student_id']
+        print(student_id)
 
         if json_token['permission'] != 1:
             return "<h1>Unauthorized access!</h1>"
+        
     except jwt.exceptions.ExpiredSignatureError:
         return "<h1>Expired session!</h1>"
         
@@ -74,9 +122,8 @@ def get_uploaded_file():
         {'question': '10. How do authentication and authorization work together in a secure environment?', 'answers': ['A. Authentication and authorization are not related', 'B. Authorization is not necessary if authentication is successful', 'C. Users must prove their identities before being granted access to requested resources', 'D. Authorization is always granted before authentication'], 'right_answer': 'C'}
     ]
 
-
-# create_quiz(quiz)
-    get_all_quizzes()
+    #! should add this 
+    insert_quiz(quiz, student_id)
 
     filename = secure_filename(uploaded_file.filename)
     uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], f'quiz-source.{filename.split(".")[-1]}'))
@@ -85,158 +132,98 @@ def get_uploaded_file():
     with open(os.path.join(app.config['UPLOAD_FOLDER'], 'quiz-source.txt'), 'w', encoding="utf-8") as f:
         f.write(text)
 
-    # generate_multiple_choice_questions()
     return redirect(url_for('home'))
 
+# ----------------------------------- #
+#     Inserts the quiz in the DB      #
+# ----------------------------------- #
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        try:
-            user = get_user(request.form['username-field'], request.form['password-field'])
-
-            token = generate_token(user[0], user[3])
-           
-            session['token'] = token
-
-            return "<h1>Successfully logged in.</h1>"
-
-        except argon2.exceptions.VerifyMismatchError:
-            return jsonify({"message": "Invalid username or password"}), 401
-    else:
-        return render_template("login.html")
-
-@app.route("/quiz/<int:quiz_id>")
-def quiz(quiz_id):
+def insert_quiz(quiz, owner_id):
     cursor = cnx.cursor()
 
-    cursor.execute("SELECT * FROM quizzes WHERE id = %s", (quiz_id,))
+    cursor.execute("INSERT INTO quiz (name, ownerId) VALUES (%s, %s)", ('Quiz Name', owner_id))
+    quiz_id = cursor.lastrowid
+
+    for i, q in enumerate(quiz):
+        cursor.execute("INSERT INTO questions (quiz_id, question_text) VALUES (%s, %s)", (quiz_id, q['question']))
+        question_id = cursor.lastrowid
+
+        for j, a in enumerate(q['answers']):
+            is_correct = (a == q['right_answer'])
+            cursor.execute("INSERT INTO options (question_id, option_text, is_correct) VALUES (%s, %s, %s)", (question_id, a, is_correct))
+
+    cnx.commit()
+    cursor.close()
+
+# ----------------------------------- #
+#  Returns a specific quiz by its id  #
+# ----------------------------------- #
+
+def get_quiz(quiz_id):
+    cursor = cnx.cursor()
+
+    cursor.execute("SELECT * FROM quiz WHERE id = %s", (quiz_id,))
     quiz_row = cursor.fetchone()
 
     if quiz_row is None:
-        return "Quiz not found", 404
+        return None
 
-    quiz = {
-        "id": quiz_row[0],
-        "title": quiz_row[1],
-        "questions": []
-    }
+    quiz = []
 
     cursor.execute("SELECT * FROM questions WHERE quiz_id = %s", (quiz_id,))
     question_rows = cursor.fetchall()
 
     for question_row in question_rows:
-        question = {
-            "id": question_row[0],
-            "question": question_row[2],
-            "options": [],
-            "correct": None
-        }
+        question = {'question': question_row[2], 'answers': [], 'right_answer': None}
 
-        cursor.execute("SELECT * FROM answers WHERE question_id = %s", (question["id"],))
-        answer_rows = cursor.fetchall()
+        cursor.execute("SELECT * FROM options WHERE question_id = %s", (question_row[0],))
+        option_rows = cursor.fetchall()
 
-        for answer_row in answer_rows:
-            question["options"].append(answer_row[2])
-            if answer_row[3]:
-                question["correct"] = answer_row[2]
+        for option_row in option_rows:
+            question['answers'].append(option_row[2])
+            if option_row[3]:
+                question['right_answer'] = option_row[2]
 
-        quiz["questions"].append(question)
+        quiz.append(question)
 
     cursor.close()
+    return quiz
+
+
+# ----------------------------------- #
+#  Gets all the quizzes by the userid #
+# ----------------------------------- #
+
+def get_quizzes_by_user(user_id):
+    cursor = cnx.cursor()
+
+    cursor.execute("SELECT * FROM quiz WHERE ownerId = %s", (user_id,))
+    quiz_rows = cursor.fetchall()
+
+    quizzes = []
+    for quiz_row in quiz_rows:
+        quiz_id = quiz_row[0]
+        quiz = get_quiz(quiz_id)
+        quizzes.append(quiz)
+
+    cursor.close()
+    return quizzes
+
+
+# ----------------------------------- #
+#  Template generator example /quiz/1 #
+#       returns the quiz with id 1    #
+# ----------------------------------- #
+
+@app.route("/quiz/<int:quiz_id>")
+def quiz(quiz_id):
+    quiz = get_quiz(quiz_id)
     return render_template('quiz.html', quiz=quiz)
 
-def get_all_quizzes():
-    cursor = cnx.cursor()
-    cursor.execute("SELECT * FROM quizzes")
-    quizzes = cursor.fetchall()
-    print(quizzes)
-    cursor.close()
-
-def get_tables():
-    cursor = cnx.cursor()
-    cursor.execute("SHOW TABLES")
-    tables = cursor.fetchall()
-    print(tables)
-    cursor.close()
-
-get_tables()
-
-# ! create quiz tables
-def create_quiz_tables():
-    cursor = cnx.cursor()
-    cursor.execute("""
-        CREATE TABLE quiz (
-            id INT AUTO_INCREMENT PRIMARY KEY, 
-            name VARCHAR(255),
-            ownerId INT,
-            FOREIGN KEY (ownerId) REFERENCES user(id)
-        )
-    """)
-
-    cursor.execute("""
-        CREATE TABLE questions (
-            id INT AUTO_INCREMENT PRIMARY KEY, 
-            quiz_id INT, 
-            question_text TEXT, 
-            FOREIGN KEY (quiz_id) REFERENCES quiz(id)
-        )
-    """)
-
-    cursor.execute("""
-        CREATE TABLE options (
-            id INT AUTO_INCREMENT PRIMARY KEY, 
-            question_id INT, 
-            option_text TEXT, 
-            is_correct BOOLEAN, 
-            FOREIGN KEY (question_id) REFERENCES questions(id)
-        )
-    """)
-
-    cursor.close()
-
-# ! create quiz insert
-def create_quiz(quiz, owner_id):
-    cursor = cnx.cursor()
-
-    cursor.execute("INSERT INTO quiz (name, ownerId) VALUES (%s, %s)", (quiz['title'], owner_id))
-    quiz_id = cursor.lastrowid
-
-    for question in quiz['questions']:
-        cursor.execute("INSERT INTO questions (quiz_id, question_text) VALUES (%s, %s)", (quiz_id, question['question']))
-        question_id = cursor.lastrowid
-
-        for option in question['options']:
-            is_correct = (option == question['correct'])
-            cursor.execute("INSERT INTO options (question_id, option_text, is_correct) VALUES (%s, %s, %s)", (question_id, option, is_correct))
-
-    cnx.commit()
-    cursor.close()
-
-
-
-def get_user(username, password):
-
-    cursor.execute("select * from Users where user_validator = %s", (username,))
-    user = cursor.fetchone()
-    print(user)
-
-    if argon2.verify_password(bytes(user[2]), password.encode('utf-8')):
-        return user
-
-    return None
-
-
-def generate_token(id, permission):
-    payload = {
-        "id": id,
-        "permission": permission,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=0.016)
-    }
-
-    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
-    return token
-
+# --------------------------------------------- #
+#    Handles quiz submitting with generating a  #
+#    file in this format quizId_studentId.txt   #
+# --------------------------------------------- #
 
 @app.route('/quiz/<int:quiz_id>/submit', methods=['POST'])
 def submit_quiz(quiz_id):
@@ -259,3 +246,40 @@ def submit_quiz(quiz_id):
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
     cnx.close()
+
+# ----------------------------------- #
+#  Used if a new database is created  #
+# ----------------------------------- #
+# def create_quiz_tables():
+#     cursor = cnx.cursor()
+#     cursor.execute("""
+#         CREATE TABLE quiz (
+#             id INT AUTO_INCREMENT PRIMARY KEY, 
+#             name VARCHAR(255),
+#             ownerId BIGINT UNSIGNED NOT NULL,
+#             FOREIGN KEY (ownerId) REFERENCES user(id)
+#         )
+#     """)
+
+#     cursor.execute("""
+#         CREATE TABLE questions (
+#             id INT AUTO_INCREMENT PRIMARY KEY, 
+#             quiz_id INT NOT NULL, 
+#             question_text TEXT, 
+#             FOREIGN KEY (quiz_id) REFERENCES quiz(id)
+#         )
+#     """)
+
+#     cursor.execute("""
+#         CREATE TABLE options (
+#             id INT AUTO_INCREMENT PRIMARY KEY, 
+#             question_id INT NOT NULL, 
+#             option_text TEXT, 
+#             is_correct BOOLEAN, 
+#             FOREIGN KEY (question_id) REFERENCES questions(id)
+#         )
+#     """)
+
+#     cursor.close()
+
+# create_quiz_tables()
