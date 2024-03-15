623141c382b67091ccc28cbf259d9fc0d4e9ebf4
Stily <simoncankov@gmail.com>
2024-03-15 10:43:29 +0200

    Remove unnecessary code and move correct_answers files

--- CreateStatistics.py
+++ CreateStatistics.py
@@ -8,7 +8,6 @@ import os
 
 
 def answers_fidelity(correctAnswersCount, IncorrectAnswersCount, save_path, i, student_id):
-    # Filter out zero values
     if correctAnswersCount == 0:
         sizes = np.array([IncorrectAnswersCount])
         labels = ['Incorrect']
@@ -23,7 +22,6 @@ def answers_fidelity(correctAnswersCount, IncorrectAnswersCount, save_path, i, s
     ax.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
     plt.title(f"Answers Fidelity {student_id}. number")
 
-    # Save the plot
     plt.savefig(save_path)
     plt.close()
 
@@ -31,7 +29,7 @@ def statistics(inputData, save_path, i):
     plt.suptitle(f'Question {i}', fontsize=18)
     plt.hist(inputData, bins=len(inputData) * 2, alpha=0.7, edgecolor='black')
     plt.yticks(np.arange(0, len(inputData) + 1, 1))
-    plt.savefig(save_path)  # Save the histogram as a PNG file
+    plt.savefig(save_path)  
     plt.close()
 
 def check_question_answers(question_answers, correct_answers):
@@ -52,15 +50,12 @@ if __name__ == '__main__':
     root.iconbitmap('statistics.png')
     root.geometry('800x400')
 
-    # Save directory for the statistics
     save_statistics_directory = 'Statistics'
 
     save_diagrams_directory = '/Diagrams'
 
-    # Specify the directory containing student answer files
-    answer_directory = '/Student_answers'
+    answer_directory = '/Student_answers/correct_answers'
 
-    # Read student answers and correct answers
     #answers = s_a.get_student_answers(answer_directory)
     correct_answers = c_a.read_correct_answers(answer_directory)
     #print('corrrr', correct_answers)
@@ -75,7 +70,6 @@ if __name__ == '__main__':
         print('aaa', correct_count, incorrect_count)
         answers_fidelity(correct_count, incorrect_count, save_path_diagrams, i, student_id)
 
-    # Iterate over each question
     # for i, question_answers in enumerate(zip(*answers)):
     #
     #     correct_count, incorrect_count = check_question_answers(question_answers, correct_answers[i])
